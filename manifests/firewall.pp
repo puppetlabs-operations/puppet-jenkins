f6f0a8f9e9b33e3395869654202b7cd8cd81b53b
Derrick Dymock <derrick@puppetlabs.com>
2013-09-12 16:53:12 -0700

    Work with our firewall setup.

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -1,10 +1,10 @@
 class jenkins::firewall {
-  if defined('::firewall') {
-    ::firewall {
+  if defined('::fw') {
+    firewall {
       '500 allow Jenkins inbound traffic':
         action => 'accept',
         state  => 'NEW',
-        dport  => [8080],
+        dport  => '8080',
         proto  => 'tcp',
     }
   }
